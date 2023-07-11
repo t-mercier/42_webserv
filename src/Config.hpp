2301b4482ec3bb559b36551bc52033edea535cb6
timothee <94700601+t-mercier@users.noreply.github.com>
2023-07-11 17:57:50 +0200

    parse started

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,8 +1,42 @@
 #pragma once
 #include <istream>
+#include <vector>
+
+enum token {
+  NONE,
+  KEY,
+  SEMI = ';',
+  LB = '{',
+  RB = '}',
+};
+
+class AST {
+ public: 
+  std::vector<AST> value;
+  std::string key;
+};
 
 class Config {
-
 public:
-  static Config &from(std::istream& s);
+  static Config &from(std::istream &s);
+};
+
+class Token {
+public:
+  Token();
+  Token(int token, std::string);
+  enum token id;
+  std::string value;
+};
+
+class Parser {
+public:
+  Parser();
+  Parser(std::istream &);
+  Token getToken();
+  AST parse();
+  char getChar();
+
+private:
+  std::istream &stream;
 };
